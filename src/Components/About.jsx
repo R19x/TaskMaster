1852e4b5d84af9b7f4a2e58f80553406e7db77b5
Rahul Gupta <rahulgupta19x@gmail.com>
2024-07-01 17:02:42 +0530

    RouterAdd

---
+++ src/Components/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='bg-blue-200'>
+        <div>Made By : Rahul Gupta</div>
+        <div>React Router used for routing between between pages through Navbar</div>
+        
+    </div>
+  )
+}
+
+export default About
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { BiTask } from "react-icons/bi";
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <div>
             <nav className='flex justify-between bg-slate-900 p-5 items-center font-mono text-white'>
                 <div className='flex items-center cursor-pointer'><BiTask size={50} color='red' /><div className='m-2 text-3xl font-bold'>TaskMaster</div></div>
+                <ul className='flex gap-5 items-center cursor-pointer'>
+                    <Link to="/TaskMaster"><li>Home</li></Link>
+                    <Link to="/About"><li>About</li></Link>
+                </ul>
             </nav>
         </div>
     )
